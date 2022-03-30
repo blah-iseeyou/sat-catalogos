c015a0423eaadf412bb4a97f141baa7a9f057d69
juanjavc06 <juan.javc06@gmail.com>
2022-03-29 17:33:14 -0700

    Catalogos iniciales

---
+++ index.js
@@ -0,0 +1,15 @@
+const c_TipoDeComprobante = require("./src/c_TipoDeComprobante.json")
+const c_RegimenFiscal  = require("./src/c_RegimenFiscal.json") 
+const c_UsoCFDI = require("./src/c_UsoCFDI.json")
+const c_Exportacion = require("./src/c_Exportacion.json")
+const c_ClaveProdServ = require("./src/c_ClaveProdServ.json")
+
+
+
+module.export = {
+    c_TipoDeComprobante,
+    c_RegimenFiscal,
+    c_UsoCFDI,
+    c_Exportacion,
+    c_ClaveProdServ
+}
